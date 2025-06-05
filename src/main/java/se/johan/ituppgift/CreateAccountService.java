45590cc274b8c5bac37e3bb79efb6e1074afca22
tHaraldsson <tommy.haraldsson@stud.sti.se>
2025-06-05 11:38:32 +0200

    ADD: LOGGER (logback.xml)

--- src/main/java/se/johan/ituppgift/CreateAccountService.java
+++ src/main/java/se/johan/ituppgift/CreateAccountService.java
@@ -1,5 +1,8 @@
 package se.johan.ituppgift;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +12,7 @@ public class CreateAccountService {
 
     private final AppUserRepository appUserRepository;
     private final PasswordEncoder passwordEncoder;
+    private static final Logger logger = LoggerFactory.getLogger(CreateAccountService.class);
 
     public CreateAccountService(AppUserRepository appUserRepository, PasswordEncoder passwordEncoder) {
         this.appUserRepository = appUserRepository;
@@ -17,13 +21,19 @@ public class CreateAccountService {
 
     @PostMapping("/new")
     public AppUser createUser(String username, String password, String role, boolean consentGiven) {
-        AppUser appUser = new AppUser();
-        appUser.setUsername(username);
-        appUser.setPassword(passwordEncoder.encode(password));
-        appUser.setRole(role);
-        appUser.setConsentGiven(consentGiven);
-        appUserRepository.save(appUser);
-
+        logger.info("Creating new user");
+        AppUser appUser = null;
+        try {
+            logger.debug("Starting task");
+            appUser = new AppUser();
+            appUser.setUsername(username);
+            appUser.setPassword(passwordEncoder.encode(password));
+            appUser.setRole(role);
+            appUser.setConsentGiven(consentGiven);
+            appUserRepository.save(appUser);
+        } catch (Exception e) {
+            logger.error("Error creating new user", e);
+        }
         return appUser;
     }
 }
